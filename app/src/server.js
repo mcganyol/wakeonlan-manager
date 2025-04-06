4a21bfcc6500d654dd1f67594b51b6769792aeae
mcganyol <szabo.zoltan.boz@gmail.com>
2025-04-06 21:44:27 +0200

    - refctor

--- app/src/server.js
+++ app/src/server.js
@@ -1,24 +1,27 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-import fs from 'fs';
-
-// Get current directory (ESM workaround)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const dataDir = path.join(__dirname, 'data');
-
-if (!fs.existsSync(dataDir)) {
-  fs.mkdirSync(dataDir, { recursive: true });
-}
-
 import express from 'express';
+import connectLivereload from 'connect-livereload'; // injects the script
+import livereload from 'livereload'; 
 import Database from 'better-sqlite3';
-import wakeOnLan from 'wake_on_lan';
 
 const app = express();
-const db = new Database('./data/wol.db');
+const db = new Database('/data/wol.db');
+
+if (process.env.NODE_ENV === 'development') {
+	// Start livereload server to watch public files
+	const liveReloadServer = livereload.createServer();
+	liveReloadServer.watch("/app"); // <-- Change if needed
+  
+	// Inject the livereload script into served pages
+	app.use(connectLivereload());
+  
+	// When server restarts, refresh browsers
+	liveReloadServer.server.once("connection", () => {
+	  setTimeout(() => {
+		liveReloadServer.refresh("/");
+	  }, 100);
+	});
+  }
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -36,7 +39,7 @@ db.prepare(`
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Wake on LAN Web App is running! yay!');
+  res.send("<html><head></head><body>Wake on LAN Web App is running! YAYSSSSS!</body></html>");
 });
 
 // Start server
